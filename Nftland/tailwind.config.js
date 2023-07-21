6fc04a3ac0906f5c7d45c2d67c28b60bac79a394
Amankumar977 <ak9330674@gmail.com>
2023-07-21 22:56:26 +0530

    Added 2 more sections

--- Nftland/tailwind.config.js
+++ Nftland/tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--gradient-color-stops))",
+      },
+    },
   },
   plugins: [],
 };
