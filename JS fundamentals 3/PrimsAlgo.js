f85d5579fa1f338fd49746b37b44f5c936b52c20
Amankumar977 <ak9330674@gmail.com>
2023-10-17 15:07:05 +0530

    main

--- JS fundamentals 3/PrimsAlgo.js
+++ JS fundamentals 3/PrimsAlgo.js
@@ -1,1 +1,14 @@
-let x;
+let x = BigInt(7985);
+let map = new Map();
+let fib = (x) => {
+  if (x <= 1n) {
+    return x;
+  }
+  if (map.has(x)) {
+    return map.get(x);
+  }
+  let fibVal = fib(x - 1n) + fib(x - 2n);
+  map.set(x, fibVal);
+  return fibVal;
+};
+console.log(fib(x));
