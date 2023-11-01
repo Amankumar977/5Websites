cdb334abbcc6dde818518f37fd1ab36e508439eb
Amankumar977 <ak9330674@gmail.com>
2023-11-01 10:13:05 +0530

    D

--- JS fundamentals 3/time.js
+++ JS fundamentals 3/time.js
@@ -1,1 +1,13 @@
-console.log((4 & 1) === 0 ? "Even" : "odd");
+let gcd = (a, b) => {
+  while (b !== 0) {
+    let reminder = a % b;
+    a = b;
+    b = reminder;
+  }
+  return a;
+};
+let num1 = 40;
+let num2 = 15;
+let gcdValue = gcd(num1, num2);
+let lcmValue = (num1 * num2) / gcdValue;
+console.log(lcmValue);
