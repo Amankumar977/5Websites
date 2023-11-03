f1b864ecbc8d02f9e231db1c9f1f2a75ddb53e85
Amankumar977 <ak9330674@gmail.com>
2023-11-03 15:09:03 +0530

    Done

--- JS fundamentals 3/time.js
+++ JS fundamentals 3/time.js
@@ -1,13 +1,17 @@
-let gcd = (a, b) => {
-  while (b !== 0) {
-    let reminder = a % b;
-    a = b;
-    b = reminder;
+let arr = [1, 2, 2, 9, 3, 2];
+let leftSum = 0;
+let totalSum = 0;
+let equilibriumIndex = -1;
+let n = arr.length;
+for (let i = 0; i < n; i++) {
+  totalSum += arr[i];
+}
+for (let i = 0; i < n; i++) {
+  totalSum -= arr[i];
+  if (leftSum === totalSum) {
+    equilibriumIndex = i;
+    break;
   }
-  return a;
-};
-let num1 = 40;
-let num2 = 15;
-let gcdValue = gcd(num1, num2);
-let lcmValue = (num1 * num2) / gcdValue;
-console.log(lcmValue);
+  leftSum += arr[i];
+}
+console.log(equilibriumIndex + 1);
