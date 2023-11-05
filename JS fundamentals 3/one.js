01c011d95eba427860c063c78890957e80299ebf
Amankumar977 <ak9330674@gmail.com>
2023-11-05 20:38:37 +0530

    done

---
+++ JS fundamentals 3/one.js
@@ -0,0 +1,7 @@
+db.sales.replaceOne(
+  { _id: ObjectId("451nsvgvs54s4d54sdgvvsd") },
+  {
+    book: "Naman",
+    price: 785,
+  }
+);
--- JS fundamentals 3/time.js
+++ JS fundamentals 3/time.js
@@ -1,17 +1,2 @@
-let arr = [1, 2, 2, 9, 3, 2];
-let leftSum = 0;
-let totalSum = 0;
-let equilibriumIndex = -1;
-let n = arr.length;
-for (let i = 0; i < n; i++) {
-  totalSum += arr[i];
+{
 }
-for (let i = 0; i < n; i++) {
-  totalSum -= arr[i];
-  if (leftSum === totalSum) {
-    equilibriumIndex = i;
-    break;
-  }
-  leftSum += arr[i];
-}
-console.log(equilibriumIndex + 1);
