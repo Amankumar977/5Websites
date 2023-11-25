7e3d00186fd5b7a4fa427e83b57655733975d1f3
Amankumar977 <ak9330674@gmail.com>
2023-11-25 19:55:46 +0530

    doone

--- practice/p.js
+++ practice/p.js
@@ -1,17 +1,11 @@
-let frequencyArr = [];
-for (let i = 0; i < 10; i++) {
-  frequencyArr[i] = 0;
+let nums = [1, 3, 4, 2, 2];
+let n = nums.length;
+for (let i = 0; i < n; i++) {
+  let idx = nums[i] - 1;
+  if (nums[idx] < 0) {
+    return nums[i];
+  } else {
+    nums[idx] = -nums[idx];
+  }
 }
-
-let nums1 = [1, 2, 2, 1];
-for (let value of nums1) {
-  frequencyArr[value] += 1;
-}
-console.log(frequencyArr);
-// let resultArr = [];
-// for (let value of nums2) {
-//   if (frequencyArr[value] > 0) {
-//     resultArr.push(value);
-//     frequencyArr[value--];
-//   }
-// }
+console.log(nums);
