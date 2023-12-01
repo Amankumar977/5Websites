5940fb48d1faafbe67019ab499b573de6f615d02
Amankumar977 <ak9330674@gmail.com>
2023-12-01 08:42:26 +0530

    Done

--- practice/p.js
+++ practice/p.js
@@ -1,15 +1,12 @@
-let nums = [1, 2, 2, 4];
-let n = nums.length;
-let target = 2;
-let idx = 0;
-let lastIndices = (nums, n, target, idx) => {
-  if (idx > n - 1) {
-    return -1;
+let a = 12;
+let b = 6;
+let gcd = (a, b) => {
+  if (b === 0) {
+    return a;
   }
-  let ans = lastIndices(nums, n, target, idx + 1);
-  if (ans === -1 && nums[idx] == target) {
-    ans = idx;
-  }
-  return ans;
+  let reminder = a % b;
+  return gcd(b, reminder);
 };
-console.log(lastIndices(nums, n, target, idx));
+let hcf = gcd(a, b);
+let lcm = (a * b) / hcf;
+console.log(lcm);
