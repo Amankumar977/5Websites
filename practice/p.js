0bb0334ad1076c7bac6f3f4b662fd192ffaf7dad
Amankumar977 <ak9330674@gmail.com>
2023-12-18 16:23:28 +0530

    Done

--- practice/p.js
+++ practice/p.js
@@ -1,24 +1,20 @@
-const os = require("os");
-console.log("Architecture: " + os.arch());
-/**
- * Architecture : x64
- * This is used so that we can know the architecture of the OS System.
- */
-console.log("Free memory: " + os.freemem());
-/**
- * Free memory 482992128
- * This is used so that we can know the free memory of the OS System.
- */
-console.log("Total Memory: " + os.totalmem());
-/**
- * Total Memory 6280437760
- * This is used so that we can know the total memory of the OS System.
- */
-console.log("OS type :" + os.platform());
-/**
- * OS type :win32
- */
-console.log("OS Platform :" + os.platform());
-/**
- * OS Platform :win32
- */
+let arr = [1, 1, 2, 2, 3, 10, 10, 10, 4, 4, 4, 5, 7, 7, 1, 2];
+let nonConsecSub = (arr) => {
+  let n = arr.length;
+  let stack = [];
+  let currValue = arr[0];
+  stack.push(currValue);
+  let currCount = 1;
+  for (let i = 1; i < n; i++) {
+    if (currValue != arr[i]) {
+      currValue = arr[i];
+      stack.push(currValue);
+      currCount = 1;
+    } else if (currCount == 1 && currValue == arr[i]) {
+      stack.pop();
+      currCount--;
+    }
+  }
+  return stack;
+};
+console.log(nonConsecSub(arr));
