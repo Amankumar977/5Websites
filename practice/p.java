73c53f0639431e53659b3d9875caa85c6c66f38d
Aman Kumar <ak9330674@gmail.com>
2024-01-17 15:02:01 +0530

    Practice something

--- practice/p.java
+++ practice/p.java
@@ -1,30 +1,18 @@
-import java.util.*;
-
-class p {
-
-	public static void main(String[] args) {
-		String s = "anagram";
-        String t = "nagaram";
-        System.out.println(isAnagram(s, t));
-	}
-    public static boolean isAnagram(String s, String t) {
-        int lengthOfs = s.length();
-        int lengthOft = t.length();
-        if(lengthOfs != lengthOft){
-            return false;
-        }
-        Map<Character, Integer> scharValues = new HashMap<>();
-        for(int i=0; i<lengthOfs; i++){
-            scharValues.put(s.charAt(i), scharValues.getOrDefault(s.charAt(i), 0)+1);
-        }
-        scharValues.size()
-        for(int i=0; i<lengthOft; i++){
-            if(!scharValues.containsKey(t.charAt(i))){
-                return false;
-            }
-            System.out.println(lengthOft--);
-        
-        }
-        return true;
+class Solution {
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        int k = flowerbed.length;
+        for (int i = 0; i < k; i++) {
+             if(i ==0){
+                 if(flowerbed[i+1] ==0){
+                     flowerbed[i]= 1;
+                     n--;
+                 }
+              }else if(i == n-1){
+                      if(flowerbed[n-2]= 0){
+                           flowerbed[n-1]= 1;
+                           n--;
+                      }
+                }
     }
-} 
+}
+}
--- practice/p.js
+++ practice/p.js
@@ -1,6 +1,25 @@
-let arr = [
-  [1, 4],
-  [2, 3],
-  [4, 6],
-  [8, 9],
-];
+let canPlaceFlowers = (flowerbed, n) => {
+  let k = flowerbed.length;
+  for (let i = 0; i < k && n > 0; i++) {
+    if (flowerbed[i] == 0) {
+      if (i == 0) {
+        if (flowerbed[i + 1] == 0) {
+          flowerbed[i] = 1;
+          n--;
+        }
+      } else if (i == k - 1) {
+        if (flowerbed[k - 2] == 0) {
+          flowerbed[k - 1] = 1;
+          n--;
+        }
+      } else if (flowerbed[i - 1] == 0 && flowerbed[i + 1] == 0) {
+        flowerbed[i] = 1;
+        n--;
+      }
+    }
+  }
+  return n == 0;
+};
+let flowerbed = [1, 0, 1, 0, 1, 0, 1];
+let n = 1;
+console.log(canPlaceFlowers(flowerbed, n));
